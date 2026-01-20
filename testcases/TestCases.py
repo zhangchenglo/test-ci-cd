5644eb31aabb0dfad5a5516c3eb02679a0343350
ceiling <zcl15093643240@outlook.com>
2026-01-20 17:38:58 +0800

    test cicd

---
+++ testcases/TestCases.py
@@ -0,0 +1,14 @@
+"""
+@Project ：test-ci-cd 
+@File    ：TestCases.py
+@IDE     ：PyCharm 
+@Author  ：ceiling
+@Date    ：2026/1/20 17:33 
+"""
+
+
+
+class TestCases:
+
+    def test_hello_world(self):
+        print("hello world")
---
+++ testcases/__init__.py
@@ -0,0 +1,7 @@
+"""
+@Project ：test-ci-cd 
+@File    ：__init__.py.py
+@IDE     ：PyCharm 
+@Author  ：ceiling
+@Date    ：2026/1/20 17:32 
+"""
